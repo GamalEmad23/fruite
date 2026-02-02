2d13c625a46999b80930e2166a5d6c6b94f84c2a
GamalEmad23 <gemad232005@gmail.com>
2026-02-02 10:18:08 +0200

    finished spalsh

--- lib/features/splash/presentation/screens/spalsh_screen.dart
+++ lib/features/splash/presentation/screens/spalsh_screen.dart
@@ -1,15 +1,31 @@
+// ignore_for_file: use_build_context_synchronously
+
 import 'package:flutter/material.dart';
+import 'package:fruit/features/onboarding/presentation/screens/onboarding_screen.dart';
 
 class SpalshScreen extends StatefulWidget {
   const SpalshScreen({super.key});
 
   @override
   State<SpalshScreen> createState() => _SpalshScreenState();
+
 }
 
 class _SpalshScreenState extends State<SpalshScreen> {
+
+@override
+void initState() {
+  super.initState();
+      Future.delayed(Duration(seconds: 3)).then(
+        (value) => Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => OnboardingScreen(),)),
+      );
+    
+}
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Image.asset("assets/images/splash.png"),
+    );
   }
 }
